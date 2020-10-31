2bd5b956781702fe5c2b8eaee37f9332ffc7ecfa
Amir Hossein Jobeiri <jobeiri_a.h@yahoo.com>
2020-10-31 03:14:38 -0700

    Adding React Icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
-function App() {
-  return <div className="App">Hello World!</div>;
-}
+// useless - work with this piece of code
+/*
+<Switch>
+  <Route path="/" />
+</Switch>;
+*/
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route path="/" />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default App;
