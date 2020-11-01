51df1babd34a42fc609d52fd46da0f0241f5796d
Amir Hossein Jobeiri <jobeiri_a.h@yahoo.com>
2020-11-01 03:47:39 -0800

    Updating the Routes pages (Problem with links)

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/pages/HomePage/Home";
-import "./App.css";
+import Services from "./components/pages/Services/Services";
+import Products from "./components/pages/Products/Products";
+import SignUp from "./components/pages/SignUp/SignUp";
 import Footer from "./components/pages/Footer/Footer";
+import "./App.css";
 
 // useless - work with this piece of code
 /*
@@ -17,7 +20,10 @@ const App = () => {
     <BrowserRouter>
       <Navbar />
       <Switch>
-        <Route path="/" exact component={Home} />
+        <Route path="/" component={Home} />
+        <Route path="/services" component={Services} />
+        <Route path="/products" component={Products} />
+        <Route path="/sign-up" component={SignUp} />
       </Switch>
       <Footer />
     </BrowserRouter>
