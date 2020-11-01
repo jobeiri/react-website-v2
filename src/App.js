7c58e659a08b42e1b7de1cd65ff86105f4e7c807
Amir Hossein Jobeiri <jobeiri_a.h@yahoo.com>
2020-11-01 01:11:00 -0800

    Adding the Footer Component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/pages/HomePage/Home";
 import "./App.css";
+import Footer from "./components/pages/Footer/Footer";
 
 // useless - work with this piece of code
 /*
@@ -18,6 +19,7 @@ const App = () => {
       <Switch>
         <Route path="/" exact component={Home} />
       </Switch>
+      <Footer />
     </BrowserRouter>
   );
 };
